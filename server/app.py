bb33890f1cb2d3380a050189975bbb75c32e6b01
manoti3 <hillarymanoti0@gmail.com>
2024-04-21 14:56:44 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -48,6 +48,32 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        if user:
+            session['user_id'] = user.id
+            return make_response(user.to_dict() 200)
+        else:
+            return {'message': 'Invalid username'}, 400
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response({'message': 'Logged out'},204)
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return make_response(user.to_dict(), 200)
+        else:
+            return make_response({}, 401)
+
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
